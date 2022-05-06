93f7f3d9f4b39e0b22c931e13580841ee1619887
Pierre GIRAUDEAU <pgiraude@e2r3p9.clusters.42paris.fr>
2022-05-06 21:48:50 +0200

    le rest du 06/05

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -6,12 +6,31 @@
 /*   By: pgiraude <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 20:25:31 by pgiraude          #+#    #+#             */
-/*   Updated: 2022/05/06 21:17:11 by pgiraude         ###   ########.fr       */
+/*   Updated: 2022/05/06 21:43:09 by pgiraude         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <strings.h>
+#include <stdio.h>
 
 void	ft_bzero(void *s, size_t n)
 {
+	int i;
 
+	while (i < n)
+	{
+		(char) s[i] = 0;
+		i++;
+	}
+}
+
+int	main(void)
+{
+	char t[] = {1, 2, 3, 4};
+
+	size_t size = sizeof(char) * 4;
+
+	ft_bzero(t, size);
+	printf("%i\n", t[0]);
+	printf("%i\n", t[1]);
 }
