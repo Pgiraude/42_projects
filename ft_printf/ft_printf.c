9d9cd6a795066cb3beed17cd2fd7b6e9e71ba16b
Pierre GIRAUDEAU <pgiraude@e1r5p23.clusters.42paris.fr>
2022-07-27 21:00:43 +0200

    minor update

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: pgiraude <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 19:18:25 by pgiraude          #+#    #+#             */
-/*   Updated: 2022/07/26 21:37:31 by pgiraude         ###   ########.fr       */
+/*   Updated: 2022/07/27 20:59:16 by pgiraude         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,32 @@
 #include <stdio.h>
 
 
+void	ft_convert_hex(int flag)
+{
+	char	*base;
+
+	base = "0123456789abcdef";
+	if (flag > 15)
+	{
+		ft_convert_hex(flag / 16);
+	}
+	ft_putchar_fd(base[flag % 16], 1);
+}
+
+
+void	ft_putchar_putnbr_u(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr_u(unsigned int flag)
+{
+	if (flag > 9)
+	{
+		ft_putnbr_u(flag / 10);
+	}
+	ft_putchar_putnbr_u(((flag % 10) + 48));
+}
 
 
 int	ft_select_format(va_list format, char flag)
@@ -30,8 +56,15 @@ int	ft_select_format(va_list format, char flag)
 	else if (flag == 's')
 		ft_putstr_fd(va_arg(format, char*), 1);
 	else if (flag == 'i')
-		ft_putnbr_fd(va_arg(format, int))
+		ft_putnbr_fd(va_arg(format, int), 1);
 	else if (flag == 'u')
+		ft_putnbr_u(va_arg(format, int));
+	else if(flag == 'c')
+		ft_putchar_fd(va_arg(format, int), 1);
+	else if (flag == 'x')
+	{
+		ft_convert_hex(va_arg(format, int));
+	}
 
 }
 
@@ -62,5 +95,6 @@ int	ft_printf(const char *str, ...)
 
 int	main()
 {
-	ft_printf("j'aime les %s, les %s et la %s surtout avec %d tomates %% %% ", "oeufs", "patates", "ratatouille", 81);
+	ft_printf("j'aime les %s, les %s et la %s surtout avec %d tomates %% %% la on est %u %i %c %x", "oeufs", "patates", "ratatouille", 81, -1, 10, 'b', 18);
+	printf("\n%u %i %c %X", -1, 10, 'b', 18);
 }
