47e88c37e57c186291949c2576ea982a06ac6323
Pierre GIRAUDEAU <pgiraude@e1r11p15.clusters.42paris.fr>
2022-08-26 21:18:54 +0200

    start

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,27 @@
 /*   By: pgiraude <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/24 21:12:47 by pgiraude          #+#    #+#             */
-/*   Updated: 2022/08/24 21:15:55 by pgiraude         ###   ########.fr       */
+/*   Updated: 2022/08/26 21:05:54 by pgiraude         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+char    *get_next_line(int fd)
+{
+    
+}
+
+int main(void)
+{
+    int fd;
+    char *result;
+
+    fd = open("TEXT", O_RDONLY);
+    result = get_next_line(fd);
+    printf("%s", result);
+}
