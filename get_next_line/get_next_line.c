b312d3043a856762e1ea64d3f0c8b1771d55b12d
Pierre GIRAUDEAU <pgiraude@e1r4p2.clusters.42paris.fr>
2022-08-29 21:51:28 +0200

    mini update

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: pgiraude <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/24 21:12:47 by pgiraude          #+#    #+#             */
-/*   Updated: 2022/08/26 21:05:54 by pgiraude         ###   ########.fr       */
+/*   Updated: 2022/08/29 21:01:35 by pgiraude         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,18 @@
 
 char    *get_next_line(int fd)
 {
+    static char buffer;
+    int reader;
+
+    reader = read()
+    if (!fd)
+        return (NULL);
+
+    buffer = malloc(sizeof(char *) * (BUFFER_SIZE + 1));
+    if (!buffer)
+        return (NULL);
     
+
 }
 
 int main(void)
@@ -26,7 +37,7 @@ int main(void)
     int fd;
     char *result;
 
-    fd = open("TEXT", O_RDONLY);
+    fd = open("TEST", O_RDONLY);
     result = get_next_line(fd);
     printf("%s", result);
 }
