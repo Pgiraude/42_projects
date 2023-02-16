3e15eae88ff83432ae09d0e9039e0470436b58e4
Pierre GIRAUDEAU <pgiraude@e1r4p8.clusters.42paris.fr>
2023-02-16 19:17:24 +0100

    push swap

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -1,14 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pgiraude <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/15 18:49:14 by pgiraude          #+#    #+#             */
-/*   Updated: 2023/02/16 13:52:40 by pgiraude         ###   ########.fr       */
+/*   Created: 2023/02/15 18:49:08 by pgiraude          #+#    #+#             */
+/*   Updated: 2023/02/16 16:01:10 by pgiraude         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
 
+# include <stdlib.h>
+# include <stdio.h>
+
+typedef struct Test
+{
+    int Hp;
+    int Mp;
+}joueur; 
+
+int main(void);
+
+#endif
