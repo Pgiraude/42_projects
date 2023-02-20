d40a6b5a2a374738f64dafa20c21d5b549346834
Pierre GIRAUDEAU <pgiraude@e1r6p14.clusters.42paris.fr>
2023-02-20 19:36:42 +0100

    update get value

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,39 +6,18 @@
 /*   By: pgiraude <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 19:03:40 by pgiraude          #+#    #+#             */
-/*   Updated: 2023/02/16 19:49:29 by pgiraude         ###   ########.fr       */
+/*   Updated: 2023/02/20 19:35:08 by pgiraude         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void    create(joueur *lol)
+int main(int argc, char **argv)
 {
-    (*lol).Hp = 12;
-    lol->Mp = 50;
+    int x;
     
+    printf("a\n");
+    printf("b\n");
+    printf("c\n");
 }
-
-
-int main(void)
-{
-    // joueur t1 = {0, 0, 0};
     
-    // create(&t1);
-    // printf("Test Hp = %d Mp = %d\n", t1.Hp, t1.Mp);
-    
-    // t1.prenom.numero = 12;
-    // printf("Test numero = %d\n", t1.prenom.numero);
-
-    List *test = create_List();
-    test = addat_List(test, 10, 0);
-    printList(test);
-    test = addat_List(test, 8, 0);
-    printList(test);
-    test = addat_List(test, 9, 1);
-    printList(test);
-
-    test = freeList(test);
-    printList(test);
-    printf("%d", lenList(test));
-}
--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,50 +6,16 @@
 /*   By: pgiraude <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 18:49:08 by pgiraude          #+#    #+#             */
-/*   Updated: 2023/02/16 19:49:20 by pgiraude         ###   ########.fr       */
+/*   Updated: 2023/02/20 19:21:51 by pgiraude         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+# include "chainlist/chainlist.h"
 # include <stdlib.h>
 # include <stdio.h>
 
-struct user
-{
-    int numero;
-};
-
-struct Test
-{
-    int Hp;
-    int Mp;
-    struct user prenom;
-};
-
-
-
-typedef struct Test joueur;
-
-
-typedef struct Cellule
-{
-    long long int data;
-    struct Cellule *next;
-}List, Cell;
-
-typedef enum
-{
-    false,
-    true
-}Bool;
-
-int     is_empty_List(List *L);
-List    *create_List(void);
-List    *addat_List(List *L, int data, int pos);
-void    printList(List *L);
-int     lenList(List *L);
-List    *freeList(List *L);
 
 #endif
