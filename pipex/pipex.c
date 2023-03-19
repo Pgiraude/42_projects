70f5f397aabf47990c63f047472b2114ee49742b
Pedrito954 <pierre_giraudeau@hotmail.fr>
2023-03-20 00:12:46 +0100

    test environement variable pipex

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -1,2 +1,14 @@
 
 #include "pipex.h"
+
+int	main(int argc, char **argv, char **envp)
+{
+	int	i;
+
+	i = 0;
+	while (envp[i])
+	{
+		ft_printf("variable d'environnement =%s\n", envp[i]);
+		i++;
+	}
+}
--- pipex/pipex.h
+++ pipex/pipex.h
@@ -2,7 +2,7 @@
 #ifndef PIPEX_H
 # define PIPEX_H
 
-
+# include "libft/include/libft.h"
 
 
 #endif
