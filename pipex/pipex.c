fe23e123f8727b16219276a415d3f9465172b49c
Pedrito954 <pierre_giraudeau@hotmail.fr>
2023-03-23 14:18:47 +0100

    test fork and pipe

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -29,14 +29,14 @@ int	main(int argc, char **argv, char **envp)
 	fd = open(argv[1], O_RDONLY);
 	if (fd < 0)
 	{
-		ft_printf("Error, cannot open %s\n", argv[1]);
+		ft_printf("Error, cannot open fd1=%s\n", argv[1]);
 		return (1);
 	}
 
 	fd2 = open(argv[2], O_WRONLY);
 	if (fd2 < 0)
 	{
-		ft_printf("Error, cannot open %s\n", argv[4]);
+		ft_printf("Error, cannot open fd2=%s\n", argv[4]);
 		return (1);
 	}
 
@@ -48,7 +48,22 @@ int	main(int argc, char **argv, char **envp)
 		// ft_printf("variable d'environnement =%s\n", envp[i]);
 		i++;
 	}
+
+	char buffer[12];
+	int	pfd[2];
+	pipe(pfd);
+
+	write(pfd[1], "test pipe\n", ft_strlen("test pipe\n"));
+	
+	read(pfd[0], buffer, 11);
+	printf("%s", buffer);
+
 	pid = fork();
+	if (pid < 0)
+	{
+		ft_printf("fork failed\n");
+		return (1);
+	}
 
 	if (pid == 0)
 	{
