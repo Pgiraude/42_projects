975e9e45d9c00860132bf7a355bae6d6abbcbfba
Pierre GIRAUDEAU <pgiraude@e1r6p10.clusters.42paris.fr>
2023-05-01 19:01:10 +0200

    fix:norminette include

--- pipex/include/pipex.h
+++ pipex/include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: pgiraude <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 19:15:13 by pgiraude          #+#    #+#             */
-/*   Updated: 2023/04/27 20:25:17 by pgiraude         ###   ########.fr       */
+/*   Updated: 2023/05/01 18:57:08 by pgiraude         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,28 +23,28 @@
 
 typedef enum e_pipe
 {
-    READ,
-    WRITE
+	READ,
+	WRITE
 }t_pipe;
 
 typedef struct e_data
 {
-    int     file1;
-    int     file2;
-    char    **env;
-    char    **options;
-    char    *path;
-    pid_t   *pid;
-    int     index_cmd;
+	int		file1;
+	int		file2;
+	char	**env;
+	char	**options;
+	char	*path;
+	pid_t	*pid;
+	int		index_cmd;
 }t_data;
 
-int     open_file(int argc, char **argv, int *first_cmd, t_data *data);
-int     get_command(char *cmd, char **envp, t_data *data, int index);
+int		open_file(int argc, char **argv, int *first_cmd, t_data *data);
+int		get_command(char *cmd, char **envp, t_data *data, int index);
 
 void	exit_clean(t_data *data, int mode);
-int     check_arguments(int argc, char **argv, int min_arg);
-int     is_here_doc(char **argv);
+int		check_arguments(int argc, char **argv, int min_arg);
+int		is_here_doc(char **argv);
 void	wait_all_child(t_data *data);
-void    error_manager(char *msg, t_data *data, int error_code);
+void	error_manager(char *msg, t_data *data, int error_code);
 
-#endif 
+#endif
