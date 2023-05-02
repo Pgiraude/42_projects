3595d6709285e9c47b6ea001417a3448549a4592
Pierre GIRAUDEAU <pgiraude@e1r4p11.clusters.42paris.fr>
2023-05-02 14:16:42 +0200

    fixe:makefile

--- so_long/src/so_long.c
+++ so_long/src/so_long.c
@@ -5,21 +5,22 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: pgiraude <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/01 16:49:11 by pgiraude          #+#    #+#             */
-/*   Updated: 2023/05/01 19:12:14 by pgiraude         ###   ########.fr       */
+/*   Created: 2023/05/02 14:03:01 by pgiraude          #+#    #+#             */
+/*   Updated: 2023/05/02 14:07:55 by pgiraude         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
+#include "../minilibx-linux/mlx.h"
 
 int	main(void)
 {
 	void	*mlx;
-	void	*mlx_win;
+	void	*img;
+	// void	*mlx_win;
 
 	mlx = mlx_init();
-	mlx_win = "ss";
-	mlx_win = "sssd";
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "test");
-	mlx_loop(mlx);
+	img = mlx_new_image(mlx, 1920, 1080);
+	// mlx_win = mlx_new_window(mlx, 1920, 1080, "test");
+	// mlx_loop(mlx);
 }
