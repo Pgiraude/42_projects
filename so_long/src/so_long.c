0374d4b54ce94bb0fe3ad06efbe84f8d97076795
Pedrito954 <pierre_giraudeau@hotmail.fr>
2023-05-14 18:52:00 +0200

    in process:create map done:print walls

--- so_long/src/so_long.c
+++ so_long/src/so_long.c
@@ -32,7 +32,7 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
-void	my_mlx_get_screen_size(t_map map_param, t_vars vars , t_data img)
+void	my_mlx_get_screen_size(t_map map_param, t_vars vars , t_data img, char *path)
 {
 	size_t	x;
 	size_t	y;
@@ -41,12 +41,36 @@ void	my_mlx_get_screen_size(t_map map_param, t_vars vars , t_data img)
 	y = 0;
 	if (map_param.map_height > 20 || map_param.map_width > 20)
 		error_manager(NULL, 40);
-	x = map_param.map_width * 50;
-	y = map_param.map_height * 50;
+	x = map_param.map_width * 16;
+	y = map_param.map_height * 16;
 	vars.mlx_win = mlx_new_window(vars.mlx, x, y, "so_long");
 	img.img = mlx_new_image(vars.mlx, x, y);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 	&img.endian);
+
+
+	int		img_width;
+	int		img_height;
+	int		*image;
+	
+	y = 0;
+
+	while (map_param.map[y])
+	{
+		x = 0;
+		while (map_param.map[y][x])
+		{
+			if (map_param.map[y][x] == '1')
+			{
+				image = mlx_xpm_file_to_image(vars.mlx, path, &img_width, &img_height);
+				mlx_put_image_to_window(vars.mlx, vars.mlx_win, image, x * 16, y * 16);
+			}
+			x++;
+		}
+		y++;
+	}
+
+	ft_printf("image width=%d height=%d\n", img_width, img_height);
 }
 
 int	main(int argc, char **argv)
@@ -65,33 +89,8 @@ int	main(int argc, char **argv)
 	if (!vars.mlx)
 		return (-1);
 	
-	my_mlx_get_screen_size(map_param, vars, img);
+	my_mlx_get_screen_size(map_param, vars, img, argv[2]);
 
-	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-	my_mlx_pixel_put(&img, 6, 5, 0x00FF0000);
-	my_mlx_pixel_put(&img, 7, 5, 0x00FF0000);
-	my_mlx_pixel_put(&img, 8, 5, 0x00FF0000);
-
-	my_mlx_pixel_put(&img, 5, 6, 0x00FF0000);
-	my_mlx_pixel_put(&img, 5, 7, 0x00FF0000);
-	my_mlx_pixel_put(&img, 5, 8, 0x00FF0000);
-	mlx_put_image_to_window(vars.mlx, vars.mlx_win, img.img, 0, 0);
-
-	char	*path;
-	int		img_width;
-	int		img_height;
-	int		*image;
-	
-	path = argv[2];
-	
-	image = mlx_xpm_file_to_image(vars.mlx, path, &img_width, &img_height);
-
-	ft_printf("image val=%d\n", image);
-
-	mlx_put_image_to_window(vars.mlx, vars.mlx_win, image, 300, 300);
-
-
-	ft_printf("image width=%d height=%d\n", img_width, img_height);
 	// mlx_hook(vars.mlx_win, 2, 1L<<0, ft_close, &vars);
 	mlx_loop(vars.mlx);
 }
