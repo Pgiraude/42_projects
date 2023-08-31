7e11388824f17256a413d2f061a715e316904d69
Pedrito954 <pierre_giraudeau@hotmail.fr>
2023-08-31 21:42:19 +0200

    pierre

--- cpp/cpp00/ex01/Contact.hpp
+++ cpp/cpp00/ex01/Contact.hpp
@@ -12,21 +12,17 @@ class Contact
 
 		Contact(void);
 		~Contact(void);
-
 		int			fill_data(std::string);
 		std::string	get_private_info(std::string);
 	
 	private:
+	
 		std::string _first_name;
 		std::string _last_name;
 		std::string _nickname;
 		std::string _phone_number;
 		std::string _darkest_secret;
 		std::string	_full;
-		
-
-
-
 };
 
 
