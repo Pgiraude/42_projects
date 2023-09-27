f2e1bc716e90458c4915d942ed80edd47e164120
Pierre GIRAUDEAU <pgiraude@made-f0Cr13s5.clusters.42paris.fr>
2023-09-27 13:51:06 +0200

    almost done 90% ex00 cpp03

--- cpp/cpp03/ex00/ClapTrap.hpp
+++ cpp/cpp03/ex00/ClapTrap.hpp
@@ -12,16 +12,28 @@ class ClapTrap
 		ClapTrap(ClapTrap const &copy);
 		~ClapTrap(void);
 
+        std::string	get_name(void) const;
+        int	get_health(void) const;
+        int	get_energy(void) const;
+
         void attack(const std::string& target);
         void takeDamage(unsigned int amount);
         void beRepaired(unsigned int amount);
 
+        ClapTrap	&operator=(ClapTrap const &rhs);
+        
+        const static int max_health = 10;
+        const static int max_energy = 10;
+
     private:
         std::string _name;
-        int         _ealth_pts;
+        int         _health_pts;
         int         _energy_pts;
         int         _attack_dmg;
     
 };
+
+std::ostream    &operator<<(std::ostream &out, const ClapTrap &i);
+
 
 #endif
